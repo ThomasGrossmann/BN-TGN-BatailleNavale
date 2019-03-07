105cbba959586581ca73e184a143fdd0a08b0cd5
ThomasGrossmann <47849503+ThomasGrossmann@users.noreply.github.com>
2019-03-07 15:58:15 +0100

    HelloWorld

---
+++ BatailleNavale/cmake-build-debug/CMakeFiles/Bataille_Navale.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Bataille_Navale.dir/main.c.obj"
+  "Bataille_Navale.pdb"
+  "Bataille_Navale.exe"
+  "Bataille_Navale.exe.manifest"
+  "libBataille_Navale.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Bataille_Navale.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
