dc39fd6fa3f10b71e56c6d3b9577b8de48c30084
ThomasGrossmann <47849503+ThomasGrossmann@users.noreply.github.com>
2019-03-07 16:55:22 +0100

    WIP

--- BatailleNavale/main.c
+++ BatailleNavale/main.c
@@ -1,3 +1,6 @@
+//Programme : BatailleNavale
+//Auteur : Thomas Grossmann
+//Date : 07.03.2019
 #include <stdio.h>
 
 int main() {
@@ -8,9 +11,20 @@ int main() {
     printf("\n1.Jouer\n2.Aide\n9.Quitter");
     scanf("%d", &Choix);
 
-    switch(Choix){
-        
-    }
+    //POUR LE MOMENT SEUL L'AFFICHAGE DE L'AIDE EST DISPONIBLE !!
 
+    switch (Choix) {
+        case 1 :
+            printf("Pas encore implementer.");
+        case 2 :
+            printf("\nLes regles du jeu sont simples. Vous et votre adversaire possedez des bateaux de tailles differentes que vous devez couler pour remporter la victoire.");
+            printf("\nVous devez les placer sur une grille de maniere strategique afin que votre adversaire ne puisse pas couler vos bateaux.");
+            printf("\nUne fois vos bateaux places, la partie peut commencer.");
+            printf("\nVous devrez choisir un endroit ou tirer en choississant une case.");
+            printf("\nExemple : vous choississez de tirer sur la case B5, vous entrerez donc : B5");
+            printf("\nRien de plus facile n'est-ce pas ?");
+            printf("\nUne fois que vous aurez tirer 2 à 3 cas s'offrent a vous. Soit votre tir en tombe a l'eau soit il a touche sa cible.");
+            printf("\nSi vous avez rater votre tir le message, A l'eau s'affiche. Dans le cas contraire Toucher ! s'affiche");
+    }
     return 0;
 }
