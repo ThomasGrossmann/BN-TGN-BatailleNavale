8ce00f3fd6cd91e9b02cc7465f22a86d137490d3
ThomasGrossmann <47849503+ThomasGrossmann@users.noreply.github.com>
2019-03-18 09:25:20 +0100

    Code

--- BatailleNavale/main.c
+++ BatailleNavale/main.c
@@ -2,9 +2,84 @@
 //Auteur : Thomas Grossmann
 //Date : 07.03.2019
 #include <stdio.h>
+#include <windows.h>
+
+#define SIZE 3
+#define STLC 218 // ┌, Single Top Left Corner
+#define STRC 191 // ┐, Single Top Right Corner
+#define SBLC 192 // └, Single Bottom Left Corner
+#define SBRC 217 // ┘, Single Bottom Right Corner
+#define SVSB 179 // │, Single Vertical Simple Border
+#define SVRB 180 // ┤, Single Vertical Right Border
+#define SVLB 195 // ├, Single Vertical Left Border
+#define SHSB 196 // ─, Single Horizontal Simple Border
+#define SHBB 193 // ┴, Single Horizontal Bottom Border
+#define SHTB 194 // ┬, Single Horizontal Top Border
+#define SC   197 // ┼, Single Center
+#define DTLC 201 // ╔, Double Top Left Corner
+#define DTRC 187 // ╗, Double Top Right Corner
+#define DBLC 200 // ╚, Double Bottom Left Corner
+#define DBRC 188 // ╝, Double Bottom Right Corner
+#define DVSB 186 // ║, Double Vertical Simple Border
+#define DVRB 185 // ╣, Double Vertical Right Border
+#define DVLB 204 // ╠, Double Vertical Left Border
+#define DHSB 205 // ═, Double Horizontal Simple Border
+#define DHBB 202 // ╩, Double Horizontal Bottom Border
+#define DHTB 203 // ╦, Double Horizontal Top Border
+#define DC   206 // ╬, Double Center
+void topborder(int width)
+{
+    printf("%c",STLC);      //Premier coin gauche
+    for (int row = 0; row < SIZE; row++)
+    {
+        printf("%c%c%c%c",SHSB,SHSB,SHSB,SHTB);
+    }
+    printf("%c%c%c%c\n",SHSB,SHSB,SHSB,STRC);
+}
+void verticalbars(int width)
+{
+    for (int row = 0; row < SIZE+2; row++)
+    {
+        printf("%c   ",SVSB);
+    }
+}
+void horizonbars(int width)
+{
+    printf("\n%c",SVLB);
+    for (int row = 0; row < SIZE; row++)
+    {
+        printf("%c%c%c%c",SHSB,SHSB,SHSB,SC);
+    }
+    printf("%c%c%c%c\n",SHSB,SHSB,SHSB,SVRB);
+}
+void bottombars(int width)
+{
+    printf("\n%c",SBLC);
+    for (int row = 0; row < SIZE; row++)
+    {
+        printf("%c%c%c%c",SHSB,SHSB,SHSB,SHBB);
+    }
+    printf("%c%c%c%c",SHSB,SHSB,SHSB,SBRC);
+}
+int grille(void)
+{
+    SetConsoleOutputCP(65001); // For accented characters
+    printf("Une grille basée sur les lignes simples:\n");
+    SetConsoleOutputCP(437); // For semi-graphic characters
+    topborder(SIZE);
+    for (int row = 0; row < SIZE; row++)
+    {
+        if (row > 0)
+        {
+            horizonbars(SIZE);
+        }
+        verticalbars(SIZE);
+    }
+    bottombars(SIZE);
+}
 
 
-int main() {
+int main(void) {
     int Choix;
 
     printf("\n---BIENVENUE DANS LA BATAILLE NAVALE---");
@@ -16,7 +91,10 @@ int main() {
 
     switch (Choix) {
         case 1 :
-            printf("Pas encore implementer.");
+            printf("Choisissez la grille predefinie (d'autres grilles viendront avec le temps");
+                if (grille = 1){
+                    grille(void);
+                }
             break;
         case 2 :
             printf("\nLes regles du jeu sont simples. Vous et votre adversaire possedez des bateaux de tailles differentes que vous devez couler pour remporter la victoire.");
